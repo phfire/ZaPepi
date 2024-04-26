13e1ee1a93f0f529fa6f78006b8e109c2ff43b76
phfire <teo.stoynov@gmail.com>
2024-04-26 20:36:44 +0300

    example with more polygons

--- image_tools.py
+++ image_tools.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image, ImageDraw, ImageChops
-from colors import sgmc_colors
+from colors import sgmc_colors,lith6_1 ,lith6_2, lith4_3
 from models import MapPoly
 
 def change_color(image: Image, color: tuple[int]):
@@ -26,22 +26,45 @@ def get_patterned_image(pattern: Image, w, h, bg_color):
 
 def get_pattern(id_pattern: int):
     folder = "images"
-    image_path = os.path.join(folder, "326.png") # os.path.join(folder, id_pattern + ".png")
+    img_name = "326.png"
+    if id_pattern == 555:
+        img_name = "326.png"
+    if id_pattern == 480:
+        img_name = "352.png"
+    else:
+        img_name = "315.png"
+    image_path = os.path.join(folder, img_name) 
     return Image.open(image_path)
 
 def get_pattern_color(id_color: int):
     if id_color in sgmc_colors:
         return sgmc_colors[id_color]
+    elif id_color in lith6_1:
+        return lith6_1[id_color]
+    elif id_color in lith6_2:
+        return lith6_2[id_color]
+    elif id_color in lith4_3:
+        return lith4_3[id_color]
     return (255, 0, 0, 255)
 
 def get_pattern_bg(id_background: int):
     if id_background in sgmc_colors:
         return sgmc_colors[id_background]
-    return (255, 255, 255, 100)
+    elif id_background in lith6_1:
+        return lith6_1[id_background]
+    elif id_background in lith6_2:
+        return lith6_2[id_background]
+    elif id_background in lith4_3:
+        return lith4_3[id_background]
+    return (255, 255, 255, 255)
 
 def create_image(polygons: list[MapPoly], w, h):
     image = Image.new('RGBA', (w, h), (255, 255, 255, 0))
-    for polygon in polygons:
+    
+
+    for i, polygon in enumerate(polygons):
+        # if i > 1:
+        #     break
         pattern = get_pattern(polygon.id_pattern)
         change_color(pattern, get_pattern_color(polygon.id_color))
         patterm_image = get_patterned_image(pattern, w, h, get_pattern_bg(polygon.id_background))
@@ -49,6 +72,5 @@ def create_image(polygons: list[MapPoly], w, h):
         ImageDraw.Draw(poly_image).polygon([tuple(x) for x in polygon.pixel_feature], outline='black', fill='white')
         composite = Image.composite(patterm_image, image, poly_image)
         image.paste(composite, (0,0), composite)
-        break
 
     image.save('output.png')
--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-
-
 from db_reader import get_polygons
 from geojson import Feature
 from turfpy.measurement import bbox
 from geometry_tools import populate_pixel_features
 from image_tools import create_image
 
-x = 2775195.38081054
-y = 5240590.731858477
+x = 2775195
+y = 5240590
 offset = 6000
+w_pixels = 2000
 
-extent = [
-    x + offset,
-    y + offset,
-    x - offset,
-    y - offset
-]
 ex = Feature(geometry={"coordinates": [
     [[x + offset, y + offset], [x + offset, y - offset],
     [x - offset, y - offset], [x - offset, y + offset],
     ]], "type": "Polygon"})
-
-polygons = get_polygons(extent)
-
-w = 2000
 raw_extent = bbox(ex)
-height = raw_extent[3] - raw_extent[1] 
-width = raw_extent[2] - raw_extent[0] 
-h = int(w * height / width)
-scale = w / width
+h_pixels = int(w_pixels * (raw_extent[3] - raw_extent[1] ) / (raw_extent[2] - raw_extent[0] ))
+scale = w_pixels / (raw_extent[2] - raw_extent[0] )
 
-polygons = populate_pixel_features(polygons, ex, w, h, scale, raw_extent)
-create_image(polygons, w, h)
+polygons = get_polygons(raw_extent)
+polygons = populate_pixel_features(polygons, ex, w_pixels, h_pixels, scale, raw_extent)
+create_image(polygons, w_pixels, h_pixels)
